f75f1ff0f3947b9af8d8ec02b9298b7b4af4808d
yuki965478 <71679934+yuki965478@users.noreply.github.com>
2020-11-11 23:39:43 +0800

    程式完成

--- AR_Midterm_107051061/Assets/Scripts/GameManager.cs
+++ AR_Midterm_107051061/Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ public class GameManager : MonoBehaviour
 {
     [Header("雛菊")]
     public Transform Deiji;
-   
+
     [Header("可可")]
     public Transform Coco;
 
@@ -15,7 +15,7 @@ public class GameManager : MonoBehaviour
     [Header("縮放"), Range(0f, 5f)]
     public float size = 0.3f;
 
-    [Header("旋轉速度"),Range(0.1f,20f)]
+    [Header("旋轉速度"), Range(0.1f, 20f)]
     public float turn = 1.5f;
 
     [Header("雛菊動畫元件")]
@@ -28,17 +28,36 @@ public class GameManager : MonoBehaviour
     {
         print("開始事件");
     }
+    //public float test = 1;
+    //public float test2 = 1;
 
     private void Update()
     {
         print("更新事件");
         print(joystick.Vertical);
 
-        Deiji.Rotate(0, joystick.Horizontal*turn, 0);
-        Coco.Rotate(0, joystick.Horizontal*turn, 0);
+        Deiji.Rotate(0, joystick.Horizontal * turn, 0);
+        Coco.Rotate(0, joystick.Horizontal * turn, 0);
 
-        Deiji.localScale += new Vector3(1, 1, 1) * joystick.Vertical*size;
+        Deiji.localScale += new Vector3(1, 1, 1) * joystick.Vertical * size;
         Coco.localScale += new Vector3(1, 1, 1) * joystick.Vertical * size;
 
+        Coco.localScale = new Vector3(1, 1, 1) * Mathf.Clamp(Coco.localScale.x, 0.5f, 3.5f);
+        Deiji.localScale = new Vector3(1, 1, 1) * Mathf.Clamp(Deiji.localScale.x, 0.5f, 3.5f);
+       //test= Mathf.Clamp(test, 0.5f, 9.9f);
+       //print(Mathf.Clamp(test2, 0, 10));
     }
+    public void Fall()
+    {
+        print("跌倒");
+        aniDeiji.SetTrigger("跌倒觸發");
+        aniCoco.SetTrigger("跌倒觸發");
+    }
+    public void PlayAnimution(string aniName)
+    {
+        print(aniName);
+        aniDeiji.SetTrigger(aniName);
+        aniCoco.SetTrigger(aniName);
+    }
+
 }
